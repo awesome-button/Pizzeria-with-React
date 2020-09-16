43616a919d48771ca45e50747cb7a309e1ea43b2
lena <oleshko.lena@gmail.com>
2020-09-16 14:15:57 +0200

    ready

---
+++ build/precache-manifest.62eeb2fa7fdfecf2ad46b885318f687e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ce584f6de20fa1936384192be18290",
+    "url": "/pizzeria-with-react/index.html"
+  },
+  {
+    "revision": "111b3213b4b2a82f3c03",
+    "url": "/pizzeria-with-react/static/css/main.b738351b.chunk.css"
+  },
+  {
+    "revision": "ea672261b4197ad6e1f0",
+    "url": "/pizzeria-with-react/static/js/2.d7ceba2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizzeria-with-react/static/js/2.d7ceba2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111b3213b4b2a82f3c03",
+    "url": "/pizzeria-with-react/static/js/main.7c630245.chunk.js"
+  },
+  {
+    "revision": "a07267b82560aa9d2d8e",
+    "url": "/pizzeria-with-react/static/js/runtime-main.cb9de30b.js"
+  },
+  {
+    "revision": "c4f0e68847ff8af16c167f9329ecab1e",
+    "url": "/pizzeria-with-react/static/media/head.c4f0e688.jpg"
+  },
+  {
+    "revision": "cb65dedc450bc66ab2d67b8d7e5af6b2",
+    "url": "/pizzeria-with-react/static/media/neutral.cb65dedc.jpg"
+  },
+  {
+    "revision": "8dcb54dd9792bdad7eab3b3ca2ca928e",
+    "url": "/pizzeria-with-react/static/media/pizza1.8dcb54dd.png"
+  },
+  {
+    "revision": "f499b4aa3e04c74d7bea2409529e10d0",
+    "url": "/pizzeria-with-react/static/media/pizza2.f499b4aa.png"
+  },
+  {
+    "revision": "054e0dca269e0ce3ad0e3449dc762de0",
+    "url": "/pizzeria-with-react/static/media/pizza3.054e0dca.png"
+  },
+  {
+    "revision": "3ab4a2040b1420fd94b1d730bf4f9ea8",
+    "url": "/pizzeria-with-react/static/media/pizza4.3ab4a204.png"
+  },
+  {
+    "revision": "21d1b398e01c7ad6fa4752f4d1e7c72e",
+    "url": "/pizzeria-with-react/static/media/pizza5.21d1b398.png"
+  },
+  {
+    "revision": "d4d134341ce681082ca169d29db2c7bc",
+    "url": "/pizzeria-with-react/static/media/pizza6.d4d13434.png"
+  },
+  {
+    "revision": "394b2cdd93ada2dbcfff94a5eb21c45c",
+    "url": "/pizzeria-with-react/static/media/pizzeria.394b2cdd.jpg"
+  },
+  {
+    "revision": "67cd818dfd60789ceb870930880044af",
+    "url": "/pizzeria-with-react/static/media/team.67cd818d.jpg"
+  }
+]);
